c330a4b2ad8260ff7a5f36ce95e58dec8e4d6402
Nick Hamnett <nick@sameoldnick.com>
2025-02-16 14:04:51 -0700

    Reproduces bug

---
+++ app/Models/Role.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\UseFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+#[UseFactory(\Database\Factories\RoleFactory::class)]
+class Role extends Model
+{
+    /** @use HasFactory<\Database\Factories\RoleFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'role',
+    ];
+}
---
+++ tests/Feature/BugTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\QueryException;
+
+class BugTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Tests creating model using UseFactory attribute and then creating a model using name guessing.
+     *
+     * @return void
+     */
+    public function test_attribute_nameguess(): void
+    {
+        try {
+            // Works fine
+            Role::factory(5)->create();
+
+            // Will try insert into roles table
+            User::factory()->create([
+                'name' => 'Test User',
+                'email' => 'test@example.com',
+            ]);
+        } catch (QueryException $e) {
+            /**
+             * This will fail with the following message:
+             *  SQLSTATE[HY000]: General error: 1 table roles has no column named name (Connection: sqlite, SQL: insert into "roles" ("name", "email", "email_verified_at", "password", "remember_token", "updated_at", "created_at") values (Test User, test@example.com, 2025-02-16 20:47:20, $2y$12$BBUBrhSB/JiTSXmvQAcGyO/ZwXjGhCMQTaaz2HcZyMsMCcU8QrC3., wuk94nSjpx, 2025-02-16 20:47:21, 2025-02-16 20:47:21))
+             */
+            $this->fail('QueryException: ' . $e->getMessage());
+        }
+    }
+}
